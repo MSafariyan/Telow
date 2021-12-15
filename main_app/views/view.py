80dc414adf09074c98485562bfc2a9765abf5b19
M.Safarian <ma.hiccup@gmail.com>
2021-12-15 13:37:02 +0330

    change query set for active deuties

--- main_app/views/view.py
+++ main_app/views/view.py
@@ -15,18 +15,17 @@ from django.db.models import Q
 
 @login_required
 def indexPanelView(request):
-    order_count = order.objects.all().count()
-    process_count = process.objects.all().count()
-    active_process = process.objects.filter(isEnable=True).count()
-    action_perms = auth_user_action.objects.filter(user_id__pk=request.user.pk).values_list('action_id', flat=True)
-    finished = status.objects.filter(status_title="پایان یافته").first()
+    order_count     = order.objects.all().count()
+    process_count   = process.objects.all().count()
+    active_process  = process.objects.filter(isEnable=True).count()
+    action_perms    = auth_user_action.objects.filter(user_id__pk=request.user.pk).values_list('action_id', flat=True)
+    finished        = status.objects.filter(status_title="پایان یافته").first()
     related_order_id_to_current_user = order_process_action.objects.filter(process_action__action__in=action_perms).filter(~Q(status=finished)).values_list('order_id').all()
-    orders = order.objects.filter(pk__in=related_order_id_to_current_user).order_by('priority').all()
+    orders          = order.objects.filter(pk__in=related_order_id_to_current_user).order_by('-priority').all()
     
-    under_check = status.objects.filter(status_title="در دست بررسی").first()
-    current_user = request.user
-    active_duties = order_process_action.objects.filter(~Q(status=finished)).filter(process_action__action__auth_user_action__user_id=current_user).values_list('order_id').all()
-    active_order_duties = order.objects.filter(pk__in=active_duties)
+    under_check     = status.objects.filter(status_title="در دست بررسی").first()
+    related_active_order_id_to_current_user = order_process_action.objects.filter(process_action__action__in=action_perms).filter(status=under_check).values_list('order_id').all()
+    active_order_duties = order.objects.filter(pk__in=related_active_order_id_to_current_user).order_by('-priority').all()
 
     return render(request, 'dashboard/dashboard.html', {"title":"پنل کاربری", "order_count":order_count, "process_count":process_count, "active_process":active_process, "active_duties":active_order_duties, "orders":orders})
     
